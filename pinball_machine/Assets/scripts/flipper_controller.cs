3360e08376d2ebca1ea86c5389abe61404a4d1ba
Zijing Liu/PC <zijingli@usc.edu>
2016-08-24 02:02:59 -0700

    flipper finished.

--- pinball_machine/Assets/scripts/flipper_controller.cs
+++ pinball_machine/Assets/scripts/flipper_controller.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class flipper_controller : MonoBehaviour {
 
 	public float torque_force;
-	public Vector3 torque_direction;
+    private int scale = 10000000;
+	public int torque_direction;
 
 	void Update()
 	{
-		//float turn = Input.GetAxis("Jump");
-		if (Input.GetButton ("Jump")) {
-			print ("button pressed\n");
-			GetComponent<Rigidbody>().AddTorque (0, -1000000,0 , ForceMode.Impulse);
-		}
-	}
+        //float turn = Input.GetAxis("Jump");
+        if (Input.GetButton("Jump"))
+        {
+            //print ("button pressed\n");
+            GetComponent<Rigidbody>().AddTorque(0, torque_direction * 10000 * torque_force, 0);
+        }
+    }
 }
