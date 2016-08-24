fc1b6870d50bc22395ad4e29d6890049d3e303e3
zijingliu <zijingli@usc.edu>
2016-08-24 14:52:32 -0700

    flippers and bouncer fixed and updated.

---
+++ pinball_machine/Assets/scripts/bouncer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncer : MonoBehaviour {
+
+	public float power;
+	public float radius;
+	public int duration; 
+	public Color onhit_color;
+	public Color original_color;
+
+	private Vector3 exposition_position;
+	private Renderer my_renderer;
+	private int ticks;
+
+	// Use this for initialization
+	void Start () {
+		exposition_position = transform.position;
+		if (gameObject.tag != "wall") {
+			my_renderer = GetComponent<Renderer> ();
+			my_renderer.material.color = original_color;
+		}
+		ticks = 0;
+	}
+
+	void Update(){
+
+		if (gameObject.tag == "wall") {
+			return;
+		}
+
+		if (ticks != 0) {
+			ticks--;
+		} 
+		else if (my_renderer.material.color != original_color) {
+			my_renderer.material.color = original_color;	
+		}
+	}
+
+	// Event for collsion
+	void OnCollisionEnter(Collision collision) {
+		collision.gameObject.GetComponent<Rigidbody> ().AddExplosionForce (power, exposition_position, radius, 3.0f);
+		if (gameObject.tag != "wall") {
+			my_renderer.material.color = onhit_color;
+			ticks = duration;
+		}
+	}
+}
--- pinball_machine/Assets/scripts/flipper_controller.cs
+++ pinball_machine/Assets/scripts/flipper_controller.cs
@@ -4,16 +4,30 @@ using System.Collections;
 public class flipper_controller : MonoBehaviour {
 
 	public float torque_force;
-    private int scale = 10000000;
+	public string key;
 	public int torque_direction;
+    
+	private int scale = 100000000;
+	private Rigidbody my_rigidbody;
+	private HingeJoint my_hingejoint;
+
+	void Start(){
+		my_rigidbody = GetComponent<Rigidbody> ();
+		my_hingejoint = GetComponent<HingeJoint> ();
+	}
+
 
 	void Update()
 	{
         //float turn = Input.GetAxis("Jump");
-        if (Input.GetButton("Jump"))
-        {
-            //print ("button pressed\n");
-            GetComponent<Rigidbody>().AddTorque(0, torque_direction * 10000 * torque_force, 0);
-        }
+		if (Input.GetButton (key)) 
+		{
+			//print ("button pressed\n");
+			my_rigidbody.AddTorque (0, torque_direction * scale * torque_force, 0);
+		} 
+		else if(my_hingejoint.angle != my_hingejoint.limits.max || my_hingejoint.angle != my_hingejoint.limits.min)
+		{
+			my_rigidbody.AddTorque (0, torque_direction * scale * torque_force * -1, 0);
+		}
     }
 }
