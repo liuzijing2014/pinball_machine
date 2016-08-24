3d3bcd80d68ddc7b41c4aa54863b5d239f3f0b09
zijingliu <zijingli@usc.edu>
2016-08-23 17:41:02 -0700

    left flipper added.

---
+++ pinball_machine/Assets/scripts/flipper_controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipper_controller : MonoBehaviour {
+
+	public float torque_force;
+	public Vector3 torque_direction;
+
+	void Update()
+	{
+		//float turn = Input.GetAxis("Jump");
+		if (Input.GetButton ("Jump")) {
+			print ("button pressed\n");
+			GetComponent<Rigidbody>().AddTorque (0, -1000000,0 , ForceMode.Impulse);
+		}
+	}
+}
