8fea86618f0399f8f8bb914d1d52f6bea57d4d0e
Zijing Liu <Zijing Liu>
2016-08-29 16:34:29 -0700

    updated.

--- pinball_machine/Assets/scripts/dropper_controller.cs
+++ pinball_machine/Assets/scripts/dropper_controller.cs
@@ -5,20 +5,26 @@ public class dropper_controller : MonoBehaviour {
 
 	public float down_distance;
 	public float down_speed;
+	public float up_speed;
 	public float viberation;
+	public float duration;
 
 	private Vector3 down_position;
+	private Vector3 original_position;
 	private bool down;
+	private float tick;
 	void Start()
 	{
-		down_position = transform.position;
+		down_position = original_position = transform.position;
 		down_position.y -= down_distance;
 		down = false;
+		tick = 0.0f;
 	}
 
 	// Event for collsion
 	void OnCollisionEnter(Collision collision) {
 		down = true;
+		tick = duration;
 		GetComponent<Collider> ().enabled = false;
 	}
 
@@ -26,10 +32,19 @@ public class dropper_controller : MonoBehaviour {
 	{
 		if (down) 
 		{
-			float random = Random.Range (-viberation, viberation);
-			Vector3 viberated_position = down_position;
-			viberated_position.x += random;
-			transform.position = Vector3.MoveTowards (transform.position, viberated_position, down_speed * Time.deltaTime);
+			if (tick >= 0.0f) {
+				float random = Random.Range (-viberation, viberation);
+				Vector3 viberated_position = down_position;
+				viberated_position.x += random;
+				transform.position = Vector3.MoveTowards (transform.position, viberated_position, down_speed * Time.deltaTime);
+				tick--;
+			}
+			else {
+				if ((transform.position = Vector3.MoveTowards (transform.position, original_position, up_speed * Time.deltaTime)) == original_position) {
+					down = false;
+					GetComponent<Collider> ().enabled = true;
+				}
+			}
 		}
 	}
 }
